9a9e55b37b1fef3a0d930e12323e1fa9c0954299
DESKTOP-U8PJLEN\DELL <hoangviet13052000@gmail.com>
2021-09-24 08:51:09 +0700

    create project

---
+++ src/main/java/com/example/jwt_nguyenhoangviet/JwtNguyenHoangVietApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_nguyenhoangviet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtNguyenHoangVietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtNguyenHoangVietApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwt_nguyenhoangviet/JwtNguyenHoangVietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_nguyenhoangviet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtNguyenHoangVietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
