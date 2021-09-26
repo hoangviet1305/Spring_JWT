49153f46624f8693c0e62ecbe594bd869989a835
DESKTOP-U8PJLEN\DELL <hoangviet13052000@gmail.com>
2021-09-26 16:54:55 +0700

    finish login_generate

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -1,8 +1,12 @@
 package controller;
 
+import Authen.UserPrincipal;
 import Service.UserService;
+import antlr.Token;
 import entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +21,19 @@ public class AuthController {
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 
         return userService.createUser(user);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){
+        UserPrincipal userPrincipal = userService.findByUserName(user.getUsername());
+        if (null == user || !new BCryptPasswordEncoder().matches(user.getPassword(),userPrincipal.getPassword())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Id or pass error");
+        }
+        Token token = new Token();
+        token.setToken(jwtUtil.generateToken(userPrincipal));
+        token.setTokenExpDate(jwtUtil.generateExpirationDate());
+        token.setCreatedBy(userPrincipal.getUserId());
+
+        return ResponseEntity.ok(token.getToken());
     }
 
 }
